742af5b9091dc4a0826eba1d8a87f6f3dbde9cbf
cilangovan <mails@cheralathan.com>
2016-04-13 16:30:54 +0100

    Signed-off-by: cilangovan <mails@cheralathan.com> CI

---
+++ DIM_CUSTOMER -SCD/DIM_CUSTOMER/BatchFileToExecuteJOB.bat
@@ -0,0 +1,3 @@
+cd N:\berk\pentaho\data-integration
+kitchen.bat /file:"N:\berk\pentaho\data-integration\JOB_LOAD_DIM_CUSTOMER.kjb" 
+pause
---
+++ DIM_CUSTOMER -SCD/DIM_CUSTOMER/BatchFileToExecuteTransformation.bat
@@ -0,0 +1,3 @@
+cd N:\berk\pentaho\data-integration
+pan.bat /file:"N:\berk\pentaho\data-integration\Load_DIM_CUSTOMER_WithSCD-2.ktr" 
+pause
